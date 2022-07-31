ac7bf6e320cce0b86d3286256a1f2bfa0d92ec87
Oleksandr-Stepanyshyn <stepanyshynoleksandr@gmail.com>
2022-07-31 18:07:18 +0300

    added subscription auth path

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 const app = require("./config/serverConfig");
-
-// Ниже импорты разных рероутов
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 const routerAuth = require("./routers/auth.routes");
 const routerTransaction = require("./routers/transaction.routes");
 
 app.use("/auth", routerAuth);
 app.use("/transaction", routerTransaction);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
